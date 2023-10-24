b30f6acfec2ed6cc9d4bb1383eb8d297c982df66
mostafa yasser <moustafa0yasser123@gmail.com>
2023-10-24 14:10:21 +0300

    add inputs number becaues show value edit and esaly edit the photo for hight qulaity

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 let saturate = document.getElementById("saturate");
+let saturateid = document.getElementById("saturateid");
 let contrast = document.getElementById("contrast");
+let contrastid = document.getElementById("contrastid");
 let brightness = document.getElementById("brightness");
+let brightnessid = document.getElementById("brightnessid");
 let sepia = document.getElementById("sepia");
+let sepiaid = document.getElementById("sepiaid");
 let grayscale = document.getElementById("grayscale");
+let grayscaleid = document.getElementById("grayscaleid");
 let blure = document.getElementById("blur");
+let blureid = document.getElementById("blurid");
 let hueRotate = document.getElementById("hue-rotate");
+let hueRotateid = document.getElementById("hue-rotateid");
+
+
+
+linkInputs(saturate, saturateid);
+linkInputs(contrast, contrastid);
+linkInputs(brightness, brightnessid);
+linkInputs(sepia, sepiaid);
+linkInputs(grayscale, grayscaleid);
+linkInputs(blure, blureid);
+linkInputs(hueRotate, hueRotateid);
+
+
+function linkInputs(range, number) {
+  range.addEventListener("input", () => {number.value = range.value; })
+  number.addEventListener("input", () => {range.value = number.value;})
+}
 
 let upload = document.getElementById("upload");
 let download = document.getElementById("download");
@@ -16,12 +39,19 @@ let mainImage = document.querySelector('.main-image');
 function  restValue() {
   ctx.filter = 'none';
   saturate.value = '100';
+  saturateid.value = '100';
   contrast.value = '100';
+  contrastid.value = '100';
   brightness.value = '100';
+  brightnessid.value = '100';
   sepia.value = '0';
+  sepiaid.value = '0';
   grayscale.value = '0';
+  grayscaleid.value = '0';
   blure.value = '0';
+  blureid.value = '0';
   hueRotate.value = '0';
+  hueRotateid.value = '0';
   ctx.drawImage(img,0,0,canvas.width,canvas.height)
 };
 rest.addEventListener('click', () => restValue())
